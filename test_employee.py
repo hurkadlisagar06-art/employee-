dff7040c7c4a0492cae766a4aedbbd44d7678cbb
hurkadlisagar06-art <hurkadlisagar06@gmail.com>
2025-12-01 15:54:02 +0530

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -4,7 +4,7 @@ def test_employee_info():
    
     expected_output = (
         "Employee Name: Alice\n"
-        "Employee ID: E123\n"
+        "Employee ID: 101\n"
         "Department: IT\n"
         "Salary: 55000"
     )
